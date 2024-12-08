82a1b88e35332ebec064b87d4b97fd5cab94191d
Siddharth Singhania <siddharthsinghania0101@gmail.com>
2024-12-08 21:38:56 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -9,10 +9,15 @@ dotenv.config({
 
 const app = express();
 
-
-
-
-connectDB();
+connectDB()   
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed!!! ",err)
+})
 
 
 
